488a6f2a854e3d4bba1de895e1d6337683e84ec3
Akash Goud Sidduluri <akashgoudsidduluri@gmail.com>
2025-09-17 15:35:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,19 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Features from "./pages/Features";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import BusSearch from "./pages/BusSearch";
 import BusDetail from "./pages/BusDetail";
-import Home from "./components/sections/Hero";
+
 const queryClient = new QueryClient();
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/buses" element={<BusSearch />} />
-        <Route path="/bus/:id" element={<BusDetail />} />
-      </Routes>
-    </Router>
-  );
-}
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
@@ -32,17 +23,18 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          {/* Main site pages */}
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/features" element={<Features />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* Your new bus-related pages */}
+          <Route path="/buses" element={<BusSearch />} />
+          <Route path="/bus/:id" element={<BusDetail />} />
+
+          {/* Catch-all route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+      </BrowserRou
